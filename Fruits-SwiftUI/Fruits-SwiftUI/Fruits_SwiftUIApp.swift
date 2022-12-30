3767047420a7ad674fcb94c75fd9f651c6196733
Noureddine Legragui <noureddinelegragui@gmail.com>
2022-12-30 14:35:35 +0100

    [1] - Setup swiftUI project

---
+++ Fruits-SwiftUI/Fruits-SwiftUI/Fruits_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_SwiftUIApp.swift
+//  Fruits-SwiftUI
+//
+//  Created by Nour-Eddine Legragui on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
